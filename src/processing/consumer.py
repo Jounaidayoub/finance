870ea365d95c84b02c5a3f1f3156b1d0da42dc34
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-05-31 23:53:49 +0100

    offset

--- src/processing/consumer.py
+++ src/processing/consumer.py
@@ -10,6 +10,7 @@ consumer = KafkaConsumer(
     'tsla-data',
     bootstrap_servers=['localhost:9092'],
     value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+    # auto_offset_reset='earliest'
 )
 
 
