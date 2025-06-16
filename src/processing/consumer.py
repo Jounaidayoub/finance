918980e083025984199006cf3f3cf9aacc75b570
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-06-16 23:02:50 +0100

    fix diagrams

--- src/processing/consumer.py
+++ src/processing/consumer.py
@@ -2,6 +2,10 @@ from kafka import KafkaConsumer,KafkaProducer
 import json
 from tasks import detect
 
+from Connection import Connection
+
+from utils import put_to_index
+
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
@@ -18,8 +22,26 @@ for message in consumer:
     
         
         
-        
+    
     detect.delay(message.value[0])
+    
+    
+    # type(detect.delay(message.value[0]))
+    
+    try:
+        put_to_index(message.value[0],client=Connection.get_elasticsearch())
+    except Exception as e:
+        print(f"Error storing  row data in Elasticsearch: {str(e)}")
+    
+    
+    # store the raw data in elasticsearch
+    
+    
+    
+    
+    
+    
+    
     # print('\a')
     print("Received message:", message.value[0], message.value[1], "ALERT" if abs(
         message.value[1]) > 1.5 else "No Alert")
