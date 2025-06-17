bec8ec45e4c20dc8baac3e139aa0ce053cc49515
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-06-17 17:31:26 +0100

    feat: added attachement in email

--- src/alerting/email_service.py
+++ src/alerting/email_service.py
@@ -1,6 +1,9 @@
 import smtplib
+import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 from dotenv import main
 import os
 
@@ -11,7 +14,7 @@ app_password = os.getenv("APP_PASSWORD")
 
 receiver_email = os.getenv("RECEIVER_EMAIL") # Make recipient configurable
 
-def send_alert_email(subject, body, recipient=None):
+def send_alert_email(subject, body, recipient=None, attachment_path=None, attachment_filename=None):
     if sender_email is None or app_password is None:
         print("❌ Error: EMAIL_ADDRESS or APP_PASSWORD environment variables not set.")
         return
@@ -24,6 +27,21 @@ def send_alert_email(subject, body, recipient=None):
     message["To"] = recipient
     message["Subject"] = subject
     message.attach(MIMEText(body, "plain"))
+
+    if attachment_path and attachment_filename:
+        try:
+            with open(attachment_path, "rb") as attachment:
+                part = MIMEBase("application", "octet-stream")
+                part.set_payload(attachment.read())
+            encoders.encode_base64(part)
+            part.add_header(
+                "Content-Disposition",
+                f"attachment; filename= {attachment_filename}",
+            )
+            message.attach(part)
+        except Exception as e:
+            print(f"❌ Error attaching file {attachment_filename}: {e}")
+            return
 
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
